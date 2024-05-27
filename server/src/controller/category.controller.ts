d8add64f115d8ffd93dab61fc7e49a89b90f0670
Kmate9806 <78857962+Kmate9806@users.noreply.github.com>
2024-05-27 21:54:13 +0200

    Delete server directory

--- server/src/controller/category.controller.ts
+++
@@ -1,7 +0,0 @@
-import { AppDataSource } from "../data-source";
-import { Category } from "../entity/Category";
-import { Controller } from "./base.controller";
-
-export class CategoryController extends Controller {
-    repository = AppDataSource.getRepository(Category);
-}
--- server/src/controller/vehicle.controller.ts
+++
@@ -1,8 +0,0 @@
-import { AppDataSource } from "../data-source";
-import { Vehicle } from "../entity/Vehicle";
-
-import { Controller } from "./base.controller";
-
-export class VehicleController extends Controller {
-    repository = AppDataSource.getRepository(Vehicle);
-}
