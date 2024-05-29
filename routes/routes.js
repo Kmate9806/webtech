c1b949333f90a94c48a40c81506e92232ef9d9d4
Kmate9806 <78857962+Kmate9806@users.noreply.github.com>
2024-05-29 19:01:25 +0200

    Delete routes directory

--- routes/routes.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-
-const router = express.Router();
-
-
-var userController = require('../src/controller/userController');
-
-router.route('/user/getAll').get(userController.getDataControllerfn);
-router.route('/user/create').post(userController.createUserControllerFn);
